158219df32843684aa3a7461007d3eda4a9920ff
borlaugg <rishabh0926@gmail.com>
2024-11-09 12:34:33 +0530

    Updated with cuda files

--- src/InOneWeekend/ray.h
+++ src/InOneWeekend/ray.h
@@ -1,36 +1,18 @@
-#ifndef RAY_H
-#define RAY_H
-//==============================================================================================
-// Originally written in 2016 by Peter Shirley <ptrshrl@gmail.com>
-//
-// To the extent possible under law, the author(s) have dedicated all copyright and related and
-// neighboring rights to this software to the public domain worldwide. This software is
-// distributed without any warranty.
-//
-// You should have received a copy (see file COPYING.txt) of the CC0 Public Domain Dedication
-// along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
-//==============================================================================================
-
+#ifndef RAYH
+#define RAYH
 #include "vec3.h"
 
+class ray
+{
+    public:
+        __device__ ray() {}
+        __device__ ray(const vec3& a, const vec3& b) { A = a; B = b; }
+        __device__ vec3 origin() const       { return A; }
+        __device__ vec3 direction() const    { return B; }
+        __device__ vec3 point_at_parameter(float t) const { return A + t*B; }
 
-class ray {
-  public:
-    ray() {}
-
-    __device__ __host__ ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
-
-    const point3& origin() const  { return orig; }
-    const vec3& direction() const { return dir; }
-
-    point3 at(double t) const {
-        return orig + t*dir;
-    }
-
-  private:
-    point3 orig;
-    vec3 dir;
+        vec3 A;
+        vec3 B;
 };
-
 
 #endif
--- src/InOneWeekend/sphere.h
+++ src/InOneWeekend/sphere.h
@@ -1,58 +1,44 @@
-#ifndef SPHERE_H
-#define SPHERE_H
-//==============================================================================================
-// Originally written in 2016 by Peter Shirley <ptrshrl@gmail.com>
-//
-// To the extent possible under law, the author(s) have dedicated all copyright and related and
-// neighboring rights to this software to the public domain worldwide. This software is
-// distributed without any warranty.
-//
-// You should have received a copy (see file COPYING.txt) of the CC0 Public Domain Dedication
-// along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
-//==============================================================================================
+#ifndef SPHEREH
+#define SPHEREH
 
-#include "hittable.h"
+#include "hitable.h"
 
-
-class sphere : public hittable {
-  public:
-    sphere(const point3& center, double radius, shared_ptr<material> mat)
-      : center(center), radius(std::fmax(0,radius)), mat(mat) {}
-
-    bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
-        vec3 oc = center - r.origin();
-        auto a = r.direction().length_squared();
-        auto h = dot(r.direction(), oc);
-        auto c = oc.length_squared() - radius*radius;
-
-        auto discriminant = h*h - a*c;
-        if (discriminant < 0)
-            return false;
-
-        auto sqrtd = std::sqrt(discriminant);
-
-        // Find the nearest root that lies in the acceptable range.
-        auto root = (h - sqrtd) / a;
-        if (!ray_t.surrounds(root)) {
-            root = (h + sqrtd) / a;
-            if (!ray_t.surrounds(root))
-                return false;
-        }
-
-        rec.t = root;
-        rec.p = r.at(rec.t);
-        vec3 outward_normal = (rec.p - center) / radius;
-        rec.set_face_normal(r, outward_normal);
-        rec.mat = mat;
-
-        return true;
-    }
-
-  private:
-    point3 center;
-    double radius;
-    shared_ptr<material> mat;
+class sphere: public hitable  {
+    public:
+        __device__ sphere() {}
+        __device__ sphere(vec3 cen, float r, material *m) : center(cen), radius(r), mat_ptr(m)  {};
+        __device__ virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const;
+        vec3 center;
+        float radius;
+        material *mat_ptr;
 };
+
+__device__ bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
+    vec3 oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = dot(oc, r.direction());
+    float c = dot(oc, oc) - radius*radius;
+    float discriminant = b*b - a*c;
+    if (discriminant > 0) {
+        float temp = (-b - sqrt(discriminant))/a;
+        if (temp < t_max && temp > t_min) {
+            rec.t = temp;
+            rec.p = r.point_at_parameter(rec.t);
+            rec.normal = (rec.p - center) / radius;
+            rec.mat_ptr = mat_ptr;
+            return true;
+        }
+        temp = (-b + sqrt(discriminant)) / a;
+        if (temp < t_max && temp > t_min) {
+            rec.t = temp;
+            rec.p = r.point_at_parameter(rec.t);
+            rec.normal = (rec.p - center) / radius;
+            rec.mat_ptr = mat_ptr;
+            return true;
+        }
+    }
+    return false;
+}
 
 
 #endif
